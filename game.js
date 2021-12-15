c061068367958b325741cd1bb8d6ce17d4ee54da
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-12-14 18:10:53 -0800

    Created Flashing System

--- game.js
+++ game.js
@@ -3,9 +3,48 @@ const topRight = document.querySelector('.top-right');
 const bottomLeft = document.querySelector('.bottom-left');
 const bottomRight = document.querySelector('.bottom-right');
 
+const getRandomPanel = () =>
+{
+  const panels = [
+    topLeft,
+    bottomRight,
+    bottomLeft,
+    topRight
+  ];
+
+  return panels[parseInt(Math.random()*panels.length)];
+}
+
 const sequence = [
-  topLeft,
-  bottomRight,
-  bottomLeft,
-  topRight
+  getRandomPanel(),
+  getRandomPanel(),
+  getRandomPanel(),
+  getRandomPanel()
 ];
+
+const flash = (panel) =>
+{
+  return new Promise((resolve, reject) =>
+  {
+    panel.className += ' active';
+    setTimeout(() =>
+    {
+      panel.className = panel.className.replace(' active', '');
+
+      setTimeout(() =>
+      {
+        resolve();
+      }, 250);
+    }, 500)
+  });
+};
+
+const main = async () =>
+{
+  for (const panel of sequence)
+  {
+    await flash(panel);
+  }
+};
+
+main();
