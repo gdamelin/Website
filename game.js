332c4021b3bc51dcd6d0975454f112b85895d546
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-12-15 14:54:53 -0800

    SIMON IS WORKING HERE

--- game.js
+++ game.js
@@ -7,12 +7,17 @@ var resetButton = document.getElementById("reset");
 var currentScore = document.getElementById("currentScore");
 var highScore = document.getElementById("highScore");
 var sequence = [];
+var playerSequence = [];
+var level = 1;
+var ready = false;
+var failed = false;
+var answerCompleted = false;
 
 startButton.addEventListener("click", startGame);
 
 function getRandomPanel()
 {
-  var panels = [topLeft, topRight, bottomLeft, bottomRight];
+  var panels = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
   var panel = panels[parseInt(Math.random()*panels.length)];
   console.log(panel);
   return(panel);
@@ -28,12 +33,12 @@ function makeSequence (num)
 
 function addFlash (panel)
 {
-  panel.classList.add("active");
+  getPanel(panel).classList.add("active");
 }
 
 function removeFlash (panel)
 {
-  panel.classList.remove("active");
+  getPanel(panel).classList.remove("active");
 }
 
 function flash (panel)
@@ -42,14 +47,122 @@ function flash (panel)
   setTimeout(() => {removeFlash(panel)}, 1000);
 }
 
-function startGame ()
+function doFlashes ()
 {
-  makeSequence(4);
   for (let i = 0; i < sequence.length; i++)
   {
     setTimeout(() => {flash(sequence[i])}, 2000*i);
     setTimeout(() => {}, 1000);
   }
+}
+
+function startGame ()
+{
+  /*while (failed != true)
+  {
+    if (sequence.length != level)
+    {
+      sequence.push(getRandomPanel());
+    }
+  }
+  */
+  if (failed != true)
+  {
+    makeSequence(4);
+    doFlashes();
+    ready = true;
+
+    if (ready == true)
+    {
+      topLeft.addEventListener("click", addTopLeftAnswer);
+      topRight.addEventListener("click", addTopRightAnswer);
+      bottomLeft.addEventListener("click", addBottomLeftAnswer);
+      bottomRight.addEventListener("click", addBottomRightAnswer);
+    }
+  }
+}
+
+function checkAnswer ()
+{
+    if (arraysEqual(playerSequence, sequence))
+    {
+      alert ("Sequence Correct!");
+      level = level+1;
+    }
+    else
+    {
+      alert ("Sequence Wrong!");
+      failed = true;
+    }
+}
+
+function addTopLeftAnswer ()
+{
+  playerSequence.push("topLeft");
+  console.log("topLeft");
+  checkIfAnswerIsDone();
+}
+
+function addTopRightAnswer ()
+{
+  playerSequence.push("topRight");
+  console.log("topRight");
+  checkIfAnswerIsDone();
+}
+
+function addBottomLeftAnswer ()
+{
+  playerSequence.push("bottomLeft");
+  console.log("bottomLeft");
+  checkIfAnswerIsDone();
+}
+
+function addBottomRightAnswer ()
+{
+  playerSequence.push("bottomRight");
+  console.log("bottomRight");
+  checkIfAnswerIsDone();
+}
+
+function checkIfAnswerIsDone ()
+{
+  if (playerSequence.length == sequence.length)
+  {
+    checkAnswer();
+  }
+}
+
+function getPanel (panel)
+{
+  if (panel == "topLeft")
+  {
+    return (topLeft);
+  }
+  if (panel == "topRight")
+  {
+    return (topRight);
+  }
+  if (panel == "bottomLeft")
+  {
+    return (bottomLeft);
+  }
+  if (panel == "bottomRight")
+  {
+    return (bottomRight);
+  }
+}
+
+function arraysEqual (playerSequence, sequence)
+{
+  for (let i = 0; i < sequence.length; i++)
+  {
+    if (playerSequence[i] != sequence[i])
+    {
+      return (false);
+    }
+  }
+
+  return (true);
 }
 
 /*
