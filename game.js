7de0bfc65f094405249f0ad432f53ca2ae69fb33
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-12-14 19:04:26 -0800

    Fixing Stuff

--- game.js
+++ game.js
@@ -1,26 +1,41 @@
-const topLeft = document.querySelector('.top-left');
-const topRight = document.querySelector('.top-right');
-const bottomLeft = document.querySelector('.bottom-left');
-const bottomRight = document.querySelector('.bottom-right');
+/**
+var topLeft = document.getElementById("topLeft");
+var topRight = document.getElementById("topRight");
+var bottomLeft = document.getElementById("bottomLeft");
+var bottomRight = document.getElementById("bottomRight");
 
-const getRandomPanel = () =>
+function getRandomPanel
 {
-  const panels = [
-    topLeft,
-    bottomRight,
-    bottomLeft,
-    topRight
-  ];
-
+  var panels = [topLeft, topRight, bottomLeft, bottomRight];
   return panels[parseInt(Math.random()*panels.length)];
 }
 
-const sequence = [
-  getRandomPanel(),
-  getRandomPanel(),
-  getRandomPanel(),
-  getRandomPanel()
-];
+var sequence [];
+
+function makeSequence (var num)
+{
+  for (let i = 0; i < num; i++)
+  {
+    sequence.add(getRandomPanel());
+  }
+}
+
+function flash (panel)
+{
+  panel.classList.add("active");
+  setTimeout(() =>
+  {
+    resolve();
+  }, 250);
+  panel.classList.remove("active");
+}
+
+for (let i = 0; i < sequence.length; i++)
+{
+  flash(sequence[i]);
+}
+
+let sequenceToGuess = [...sequence];
 
 const flash = (panel) =>
 {
@@ -39,12 +54,37 @@ const flash = (panel) =>
   });
 };
 
+let canClickPanel = false;
+
+const panelClicked = (panel) =>
+{
+  if (!canClick)
+  {
+    return;
+  }
+  const expectedPanel = sequencetoGuess.shift();
+  if (expectedPanel === panelClicked)
+  {
+    if (sequenceToGuess.length === 0)
+    {
+      //start new round
+    }
+    else
+    {
+      //end game
+      alert('Game over!');
+    }
+  }
+}
+
 const main = async () =>
 {
   for (const panel of sequence)
   {
     await flash(panel);
   }
+  canClick = true;
 };
 
 main();
+*/
