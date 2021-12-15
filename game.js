33ae3ba12c135ac9a46577e4df3cf1d196cffb30
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-12-15 11:40:56 -0800

    Made StartGame Button

--- game.js
+++ game.js
@@ -1,3 +1,57 @@
+var topLeft = document.getElementById("topLeft");
+var topRight = document.getElementById("topRight");
+var bottomLeft = document.getElementById("bottomLeft");
+var bottomRight = document.getElementById("bottomRight");
+var startButton = document.getElementById("start");
+var resetButton = document.getElementById("reset");
+var currentScore = document.getElementById("currentScore");
+var highScore = document.getElementById("highScore");
+var sequence = [];
+
+startButton.addEventListener("click", startGame);
+
+function getRandomPanel()
+{
+  var panels = [topLeft, topRight, bottomLeft, bottomRight];
+  var panel = panels[parseInt(Math.random()*panels.length)];
+  console.log(panel);
+  return(panel);
+}
+
+function makeSequence (num)
+{
+  for (let i = 0; i < num; i++)
+  {
+    sequence.push(getRandomPanel());
+  }
+}
+
+function addFlash (panel)
+{
+  panel.classList.add("active");
+}
+
+function removeFlash (panel)
+{
+  panel.classList.remove("active");
+}
+
+function flash (panel)
+{
+  addFlash(panel);
+  setTimeout(() => {removeFlash(panel)}, 1000);
+}
+
+function startGame ()
+{
+  makeSequence(4);
+  for (let i = 0; i < sequence.length; i++)
+  {
+    setTimeout(() => {flash(sequence[i])}, 2000*i);
+    setTimeout(() => {}, 1000);
+  }
+}
+
 /*
 var topLeft = document.getElementById("topLeft");
 var topRight = document.getElementById("topRight");
