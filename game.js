f6c2514b74f81cbcdd1257693cd265035edb2040
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-12-14 17:55:23 -0800

    Added some JavaScript

---
+++ game.js
@@ -0,0 +1,11 @@
+const topLeft = document.querySelector('.top-left');
+const topRight = document.querySelector('.top-right');
+const bottomLeft = document.querySelector('.bottom-left');
+const bottomRight = document.querySelector('.bottom-right');
+
+const sequence = [
+  topLeft,
+  bottomRight,
+  bottomLeft,
+  topRight
+];
