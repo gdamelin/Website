6e7255619cf3552746b80e0a1c7b344fe36640ec
benv123 <89551265+benv123@users.noreply.github.com>
2022-01-11 17:38:04 -0800

    Greg's code refurbished into organized JSON object

--- game.js
+++ game.js
@@ -4,120 +4,121 @@ let Game={
     topRight:document.getElementById("topRight"),
     bottomLeft:document.getElementById("bottomLeft"),
     bottomRight:document.getElementById("bottomRight"),
-    startButton:document.getElementById("start"),
-    resetButton:document.getElementById("reset"),
+    start:document.getElementById("start"),
+    reset:document.getElementById("reset"),
     currentScore:document.getElementById("currentScore"),
     highScore:document.getElementById("highScore")
   },
   sequencing:{
     sequence:[],
     makeSequence:function(num){
-      for(let i=0;i<num;i++){
-        sequence.push(getRandomPanel());
+      for (let i=0;i<num;i++){
+        this.sequence.push(this.getRandomPanel());
       }
     },
     getRandomPanel:function(){
-      let panels=["topLeft", "topRight", "bottomLeft", "bottomRight"];
-      let panel = panels[parseInt(Math.random()*panels.length)];
+      let panels=["topLeft","topRight","bottomLeft","bottomRight"];
+      let panel=panels[parseInt(Math.random()*panels.length)];
       console.log(panel);
       return(panel);
-    },
+    }
   },
   flashing:{
+    flash:function(panel){
+      this.addFlash(panel);
+      setTimeout(()=>{this.removeFlash(panel)},1000);
+    },
     addFlash:function(panel){
-      getPanel(panel).classList.add("active");
+      this.getPanel(panel).classList.add("active");
+    },
+    getPanel:function(panel){
+      if (panel=="topLeft"){
+        return(Game.button.topLeft);
+      }
+      if(panel=="topRight"){
+        return(topRight);
+      }
+      if (panel=="bottomLeft"){
+        return(bottomLeft);
+      }
+      if (panel=="bottomRight"){
+        return(bottomRight);
+      }
     },
     removeFlash:function(panel){
-      getPanel(panel).classList.remove("active");
+      this.getPanel(panel).classList.remove("active");
     },
-    flash:function(panel){
-      addFlash(panel);
-      setTimeout(()=>{removeFlash(panel)},1000);
-    },
-    doFlashses:function(){
-      for(let i=0;i<sequence.length;i++){
-        setTimeout(()=>{flash(sequence[i])},2000*i);
+    doFlashes:function(){
+      for (let i=0;i<Game.sequencing.sequence.length;i++){
+        setTimeout(()=>{this.flash(Game.sequencing.sequence[i])},2000*i);
         setTimeout(()=>{},1000);
       }
     }
   },
   playing:{
+    answerCompleted:false,
+    playerSequence:[],
     level:1,
     ready:false,
     failed:false,
-    answerCompleted:false,
-    playerSequence:[],
     startGame:function(){
-      if (failed!=true){
-        makeSequence(4);
-        doFlashes();
-        ready=true;
-        if(ready==true){
-          topLeft.addEventListener("click",addTopLeftAnswer);
-          topRight.addEventListener("click",addTopRightAnswer);
-          bottomLeft.addEventListener("click",addBottomLeftAnswer);
-          bottomRight.addEventListener("click",addBottomRightAnswer);
+      if (this.failed!=true){
+        Game.sequencing.makeSequence(4);
+        Game.flashing.doFlashes();
+        this.ready=true;
+        if (this.ready==true){
+          Game.button.topLeft.addEventListener("click",Game.answers.addTopLeft);
+          Game.button.topRight.addEventListener("click",Game.answers.addTopRight);
+          Game.button.bottomLeft.addEventListener("click",Game.answers.addBottomLeft);
+          Game.button.bottomRight.addEventListener("click",Game.answers.addBottomRight);
         }
       }
     },
-    addTopLeftAnswer:function(){
-      playerSequence.push("topLeft");
-      console.log("topLeft");
-      checkIfAnswerIsDone();
-    },
-    addTopRightAnswer:function(){
-      playerSequence.push("topRight");
-      console.log("topRight");
-      checkIfAnswerIsDone();
-    },
-    addBottomLeftAnswer:function()
-    {
-      playerSequence.push("bottomLeft");
-      console.log("bottomLeft");
-      checkIfAnswerIsDone();
-    },
-    addBottomRightAnswer:function(){
-      playerSequence.push("bottomRight");
-      console.log("bottomRight");
-      checkIfAnswerIsDone();
+    checkAnswer:function(){
+        if (arraysEqual(this.playerSequence,this.sequence)){
+          alert("Sequence Correct!");
+          this.level=this.level+1;
+        }
+        else{
+          alert("Sequence Wrong!");
+          this.failed=true;
+        }
     },
     checkIfAnswerIsDone:function(){
-      if(playerSequence.length==sequence.length){
-        checkAnswer();
-      }
-    },
-    getPanel:function(panel){
-      if(panel=="topLeft"){
-        return(topLeft);
-      }
-      if(panel=="topRight"){
-        return(topRight);
-      }
-      if(panel=="bottomLeft"){
-        return(bottomLeft);
-      }
-      if(panel=="bottomRight"){
-        return(bottomRight);
+      if(this.playerSequence.length==this.sequence.length){
+        this.checkAnswer();
       }
     },
     arraysEqual:function(playerSequence,sequence){
       for(let i=0;i<sequence.length;i++){
-        if(playerSequence[i]!=sequence[i]){
-          return(false);
+        if (playerSequence[i]!=sequence[i]){
+          return (false);
         }
       }
       return (true);
+    }
+  },
+  answers:{
+    addTopLeft:function(){
+      this.playerSequence.push("topLeft");
+      console.log("topLeft");
+      checkIfAnswerIsDone();
     },
-    checkAnswer:function(){
-        if(arraysEqual(playerSequence,sequence)){
-          alert("Sequence Correct!");
-          level=level+1;
-        }
-        else{
-          alert("Sequence Wrong!");
-          failed=true;
-        }
+    addTopRight:function(){
+      this.playerSequence.push("topRight");
+      console.log("topRight");
+      checkIfAnswerIsDone();
+    },
+    addBottomLeft:function(){
+      this.playerSequence.push("bottomLeft");
+      console.log("bottomLeft");
+      checkIfAnswerIsDone();
+    },
+    addBottomRight:function(){
+      this.playerSequence.push("bottomRight");
+      console.log("bottomRight");
+      checkIfAnswerIsDone();
     }
   }
 }
-startButton.addEventListener("click", startGame);
+Game.button.start.addEventListener("click",Game.playing.startGame);
